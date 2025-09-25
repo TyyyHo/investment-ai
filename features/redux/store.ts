c4253301131b4a2ae368f7972b48dfd7f8784a1e
Tyler <ty.ho1126@gmail.com>
2025-09-25 11:04:40 +0800

    feat: add counter slice and integrate into Redux store

--- features/redux/store.ts
+++ features/redux/store.ts
@@ -1,11 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "@/features/redux/stores/counterSlice";
 
 export const makeStore = () =>
   configureStore({
-    reducer: {},
-    devTools: process.env.NODE_ENV !== 'production',
+    reducer: {
+      counter: counterReducer,
+    },
+    devTools: process.env.NODE_ENV !== "production",
   });
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
